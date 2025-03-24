775249a57494eab19382d7192ef1642608312eea
Naufaldi Alfaghani <naufaldialfaghani25092005@gmail.com>
2025-03-24 10:14:11 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -23,31 +23,31 @@ public class Main {
         System.out.println();
 
         // Simulasi perjalanan Bus
-        System.out.println(GREEN + "--- Dear Passengers, Welcome On Board, This Bus Is Bound for Terminal B ---" + WHITE);
+        System.out.println(GREEN + "Dear Passengers, Welcome On Board, This Bus Is Bound for Terminal B" + WHITE);
         bus.startJourney(); // Memulai perjalanan bus
         bus.boardPassengers(30); // 30 penumpang naik
         bus.boardPassengers(20); // 20 penumpang naik lagi (kapasitas penuh: 50)
-        bus.calculateFare(); // Menghitung tarif perjalanan bus
+        bus.calculateFare(5); // Menghitung tarif perjalanan bus
         bus.stopJourney(); // Mengakhiri perjalanan bus
         bus.dropPassengers(50); // Semua penumpang turun
-        System.out.println(GREEN + "--- Thank you for your trust in this TransJakarta ---> \n" + WHITE);
+        System.out.println(GREEN + "Thank you for your trust in this TransJakarta\n" + WHITE);
 
         // Simulasi perjalanan Kereta
-        System.out.println(GREEN + "--- Dear Passengers, Welcome On Board, This Train Is Bound For Stasiun Pusat" + WHITE);
+        System.out.println(GREEN + "Dear Passengers, Welcome On Board, This Train Is Bound For Stasiun Pusat" + WHITE);
         train.startJourney(); // Memulai perjalanan kereta
         train.boardPassengers(150); // 150 penumpang naik
         train.calculateFare(); // Menghitung tarif perjalanan kereta
         train.stopJourney(); // Mengakhiri perjalanan kereta
         train.dropPassengers(150); // Semua penumpang turun
-        System.out.println(GREEN + "--- Thank You for your trust in this Commuter Line Train --- \n" + WHITE);
+        System.out.println(GREEN + "Thank You for your trust in this Commuter Line Train\n" + WHITE);
 
         // Simulasi perjalanan Taksi
-        System.out.println(GREEN + "--- Hello Welcome To This Taxi Enjoy Your Ride ---" + WHITE);
+        System.out.println(GREEN + "Hello Welcome To This Taxi Enjoy Your Ride" + WHITE);
         taxi.startJourney(); // Memulai perjalanan taksi
-        taxi.boardPassengers(3); // 3 penumpang naik
-        taxi.calculateFare(); // Menghitung tarif perjalanan taksi
+        taxi.boardPassengers(4); // 3 penumpang naik
+        taxi.calculateFare(10); // Menghitung tarif perjalanan taksi
         taxi.stopJourney(); // Mengakhiri perjalanan taksi
-        taxi.dropPassengers(3); // Semua penumpang turun
-        System.out.println(GREEN + "--- Thank You For Your Trust To This BlueBird Taxi --- \n" + WHITE);
+        taxi.dropPassengers(4); // Semua penumpang turun
+        System.out.println(GREEN + "Thank You For Your Trust To This BlueBird Taxi\n" + WHITE);
     }
 }
